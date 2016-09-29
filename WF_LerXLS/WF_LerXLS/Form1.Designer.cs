6862062c01370e5edacaf5eee322e813a95a25d4
adrigrace <adrigraceg@gmail.com>
2016-09-29 07:51:38 -0300

    .

--- WF_LerXLS/WF_LerXLS/Form1.Designer.cs
+++ WF_LerXLS/WF_LerXLS/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -37,22 +36,13 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Arquivo a ser lido";
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(24, 32);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(168, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Selecionar";
+            this.button1.Text = "Selecione o Arquivo a Ser Lido";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -60,20 +50,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 136);
+            this.ClientSize = new System.Drawing.Size(401, 206);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Leitura de Arquivo xls";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
     }
 }
--- WF_LerXLS/WF_LerXLS/Form1.cs
+++ WF_LerXLS/WF_LerXLS/Form1.cs
@@ -25,7 +25,7 @@ namespace WF_LerXLS
         private void button1_Click(object sender, EventArgs e)
         {
             Stream myStream = null;
-            bool leitura = false;
+            string leitura = "Falha na leitura";
                      
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
@@ -34,137 +34,20 @@ namespace WF_LerXLS
             openFileDialog1.FilterIndex = 2;
             openFileDialog1.RestoreDirectory = true;
 
-            DataTable dtSemDup;
-            DataTable dtDuplic;
-
+     
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
                     if ((myStream = openFileDialog1.OpenFile()) != null)
                     {
-                        
-                        Excel.Application xlApp;
-                        Excel.Workbook xlWorkBook;
-                        Excel.Worksheet xlWorkSheet;
-                        Excel.Range range;
+                        leitura = LerExcel(openFileDialog1.FileName, Path.GetExtension(openFileDialog1.FileName), 
+                            openFileDialog1.FileName.Replace(Path.GetExtension(openFileDialog1.FileName),""));
 
-                        int rCnt = 0;
-                        int cCnt = 0;
-
-                        xlApp = new Excel.Application();
-                        xlWorkBook = xlApp.Workbooks.Open(openFileDialog1.FileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-                        xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-                        range = xlWorkSheet.UsedRange;
-
-                        DataSet ds = new DataSet();
-                        foreach (Excel.Worksheet sheet in xlWorkBook.Sheets)
+                        if (leitura != "")
                         {
-                            DataTable dt = new DataTable(sheet.Name);
-
-                            for (rCnt = 1; rCnt <= range.Rows.Count; rCnt++)
-                            {
-                                for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
-                                {
-                                    DataColumn myDataColumn;
-
-                                    myDataColumn = new DataColumn();
-                                    try
-                                    {
-                                        Type typex = xlWorkSheet.Cells[2, cCnt].Value.GetType();
-                                        myDataColumn.ColumnName = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
-                                        myDataColumn.DataType = Type.GetType(typex.ToString());
-                                    }
-                                    catch
-                                    {
-                                        myDataColumn.ColumnName = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
-                                        myDataColumn.DataType = Type.GetType("System.String");
-                                    }
-
-                                    dt.Columns.Add(myDataColumn);
-                                }
-                                break;
-                            }
-
-                            for (rCnt = 2; rCnt <= range.Rows.Count; rCnt++)
-                            {
-                                DataRow rnew = dt.NewRow();
-                                for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
-                                {
-
-                                    Type typex = null;
-                                    try
-                                    {
-                                        typex = xlWorkSheet.Cells[rCnt, cCnt].Value.GetType();
-                                    }
-                                    catch
-                                    {
-                                        typex = Type.GetType("System.String");
-                                    }
-
-
-                                    if (typex.FullName.ToString() == "System.String")
-                                    {
-                                        try
-                                        {
-                                            rnew[cCnt - 1] = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
-                                        }
-                                        catch
-                                        {
-                                            
-                                        }
-                                    }
-
-                                    if (typex.FullName.ToString() == "System.Double")
-                                    {
-                                        try
-                                        {
-                                            rnew[cCnt - 1] = (double)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
-                                        }
-                                        catch
-                                        {
-                                            //rnew[cCnt - 1] = 0;
-                                        }
-                                    }
-
-                                    if (typex.FullName.ToString() == "System.DateTime")
-                                    {
-                                        try
-                                        {
-                                            rnew[cCnt - 1] = DateTime.FromOADate((range.Cells[rCnt, cCnt] as Excel.Range).Value2);
-                                        }
-                                        catch
-                                        {
-                                            //rnew[cCnt - 1] = 
-                                        }
-                                    }
-                                }
-                                dt.Rows.Add(rnew);
-                            }
-                            ds.Tables.Add(dt);
-
-                            dt.WriteXml(@"C:\\APLICACOES\\ADRI.xml");
-
-                            DataTable dt1;
-                            dt1 = dt.Clone();
-                            dt1 = dt.Copy();
-
-                            dtSemDup = RemoveDuplicateRows(dt, 1);
-                            dtDuplic = RemoveRows(dt1, 1);
-                            
-                            dtSemDup.WriteXml(@"C:\\APLICACOES\\ADRI1.xml");
-                            dtDuplic.WriteXml(@"C:\\APLICACOES\\ADRI2.xml");
-                            break;
+                            MessageBox.Show(leitura);
                         }
-
-                        xlWorkBook.Close(true, null, null);
-                        xlApp.Quit();
-
-                        releaseObject(xlWorkSheet);
-                        releaseObject(xlWorkBook);
-                        releaseObject(xlApp);
-                        leitura = true;
                     }
                 }
                 catch (Exception ex)
@@ -172,12 +55,189 @@ namespace WF_LerXLS
                     MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
                 }
             }
-            if (leitura == true)
+            if (leitura == "")
             {
                 MessageBox.Show("Leitura concluída com sucesso");
             }
         }
 
+        public string LerExcel(string NomeArquivo, string extensao, string nomesemextensao)
+        {
+            try 
+            {
+			    Excel.Application xlApp;
+			    Excel.Workbook xlWorkBook;
+			    Excel.Worksheet xlWorkSheet;
+			    Excel.Range range;
+
+			    DataTable dtSemDup;
+			    DataTable dtDuplic;
+
+			    int rCnt = 0;
+			    int cCnt = 0;
+
+			    xlApp = new Excel.Application();
+			    xlWorkBook = xlApp.Workbooks.Open(NomeArquivo, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+			    xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+			    range = xlWorkSheet.UsedRange;
+
+			    DataSet ds = new DataSet();
+			    foreach (Excel.Worksheet sheet in xlWorkBook.Sheets)
+			    {
+				    DataTable dt = new DataTable(sheet.Name);
+
+				    for (rCnt = 1; rCnt <= range.Rows.Count; rCnt++)
+				    {
+					    for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
+					    {
+						    DataColumn myDataColumn;
+
+						    myDataColumn = new DataColumn();
+						    try
+						    {
+							    Type typex = xlWorkSheet.Cells[2, cCnt].Value.GetType();
+							    myDataColumn.ColumnName = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+							    myDataColumn.DataType = Type.GetType(typex.ToString());
+						    }
+						    catch
+						    {
+							    myDataColumn.ColumnName = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+							    myDataColumn.DataType = Type.GetType("System.String");
+						    }
+
+						    dt.Columns.Add(myDataColumn);
+					    }
+					    break;
+				    }
+
+				    for (rCnt = 2; rCnt <= range.Rows.Count; rCnt++)
+				    {
+					    DataRow rnew = dt.NewRow();
+					    for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
+					    {
+
+						    Type typex = null;
+						    try
+						    {
+							    typex = xlWorkSheet.Cells[rCnt, cCnt].Value.GetType();
+						    }
+						    catch
+						    {
+							    typex = Type.GetType("System.String");
+						    }
+
+
+						    if (typex.FullName.ToString() == "System.String")
+						    {
+							    try
+							    {
+								    rnew[cCnt - 1] = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+							    }
+							    catch
+							    {
+
+							    }
+						    }
+
+						    if (typex.FullName.ToString() == "System.Double")
+						    {
+							    try
+							    {
+								    rnew[cCnt - 1] = (double)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+							    }
+							    catch
+							    {
+
+							    }
+						    }
+
+						    if (typex.FullName.ToString() == "System.DateTime")
+						    {
+							    try
+							    {
+								    rnew[cCnt - 1] = DateTime.FromOADate((range.Cells[rCnt, cCnt] as Excel.Range).Value2);
+							    }
+							    catch
+							    {
+
+							    }
+						    }
+					    }
+					    dt.Rows.Add(rnew);
+				    }
+				    ds.Tables.Add(dt);
+
+				    DataTable dt1;
+				    dt1 = dt.Clone();
+				    dt1 = dt.Copy();
+
+				    dtSemDup = RemoveDuplicateRows(dt, 1);
+				    dtDuplic = RemoveRows(dt1, 1);
+
+                    CriarExcel(nomesemextensao+"_resultado"+extensao, dtSemDup);
+                    CriarExcel(nomesemextensao + "_duplicado" + extensao, dtDuplic);
+                    break;
+			    }
+
+			    xlWorkBook.Close(true, null, null);
+			    xlApp.Quit();
+
+			    releaseObject(xlWorkSheet);
+			    releaseObject(xlWorkBook);
+			    releaseObject(xlApp);
+			    return "";
+			}
+            catch
+            {
+                return "houve uma falha";
+            }
+        }
+
+        public string CriarExcel(string NomeArquivo, DataTable DtConteudo)
+        {
+            try
+            {
+                Excel.Application xlApp;
+			    Excel.Workbook xlWorkBook;
+			    Excel.Worksheet xlWorkSheet;
+
+                xlApp = new Excel.Application();
+ 
+                object misValue = System.Reflection.Missing.Value;
+ 
+                xlWorkBook = xlApp.Workbooks.Add(System.Reflection.Missing.Value); 
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Sheets["Plan1"]; 
+ 
+                int iLinha = 0;
+                foreach (DataRow Dr in DtConteudo.Rows)
+                {
+                    iLinha = iLinha + 1;
+                    int iColuna = 0;
+                    foreach (DataColumn Dc in DtConteudo.Columns)
+                    {
+                        iColuna = iColuna + 1;
+                        xlWorkSheet.Cells[iLinha, iColuna] = Dr[Dc.ColumnName];
+                    }
+                }
+
+                xlWorkBook.SaveAs(NomeArquivo, Excel.XlFileFormat.xlWorkbookNormal,  misValue, misValue, false, misValue,  Excel.XlSaveAsAccessMode.xlNoChange, misValue, misValue, misValue,  misValue, misValue);
+                xlWorkBook.Close(true, misValue, misValue);
+ 
+                xlApp.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlApp);
+             }
+             catch (Exception e)
+             {
+                return "Houve um erro na criação do arquivo. Consulte o administrador do sistema. n" + e.Message;
+             }
+ 
+            return "";
+        }
+ 
         public DataTable RemoveDuplicateRows(DataTable dTable, int indice)
         {
             Hashtable hTable = new Hashtable();
